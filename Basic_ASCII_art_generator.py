2c49393ebb49d12992274884690e11fc88c9d132
Martin <mpoubelle34@gmail.com>
2024-01-29 19:43:48 +0100

    adding the variable scaling factor to change the number of characters displayed

--- Basic_ASCII_art_generator.py
+++ Basic_ASCII_art_generator.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 ASCII_CHARS = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.']
 
-def resize_image(image, new_width=100):
+def resize_image(image, new_width=50):
     width, height = image.size
     ratio = height / width / 1.65
     new_height = int(new_width * ratio)
@@ -13,14 +13,14 @@ def grayify(image):
     grayscale_image = image.convert("L")
     return grayscale_image
 
-def pixels_to_ascii(image):
+def pixels_to_ascii(image, scaling_factor=35):
     pixels = image.getdata()
     ascii_str = ''
     for pixel in pixels:
-        ascii_str += ASCII_CHARS[pixel // 25]
+        ascii_str += ASCII_CHARS[pixel // scaling_factor]
     return ascii_str
 
-def image_to_ascii(file_path, new_width=100):
+def image_to_ascii(file_path, new_width=100, scaling_factor=35):
     try:
         image = Image.open(file_path)
     except Exception as e:
@@ -30,7 +30,7 @@ def image_to_ascii(file_path, new_width=100):
     image = resize_image(image, new_width)
     image = grayify(image)
     
-    ascii_str = pixels_to_ascii(image)
+    ascii_str = pixels_to_ascii(image, scaling_factor)
     img_width = image.width
     
     ascii_str_len = len(ascii_str)
@@ -42,11 +42,10 @@ def image_to_ascii(file_path, new_width=100):
         f.write(ascii_img)
     return ascii_img
 
-def main(new_width=100):
+def main(new_width=100, scaling_factor=35):
     path = "dog.png"
-    ascii_art = image_to_ascii(path, new_width=new_width)
+    ascii_art = image_to_ascii(path, new_width=new_width, scaling_factor=scaling_factor)
     print(ascii_art)
-
 
 if __name__ == '__main__':
     main()
