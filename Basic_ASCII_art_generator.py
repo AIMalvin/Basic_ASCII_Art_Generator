be8c30044f120cf757b68b20f516c08e2e5e7ea7
AIMalvin <malvin.noel@edu.devinci.fr>
2024-01-29 18:07:56 +0100

    First push of the project

---
+++ Basic_ASCII_art_generator.py
@@ -0,0 +1,52 @@
+from PIL import Image
+
+ASCII_CHARS = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.']
+
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height / width / 1.65
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return resized_image
+
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return grayscale_image
+
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    ascii_str = ''
+    for pixel in pixels:
+        ascii_str += ASCII_CHARS[pixel // 25]
+    return ascii_str
+
+def image_to_ascii(file_path, new_width=100):
+    try:
+        image = Image.open(file_path)
+    except Exception as e:
+        print(e)
+        return
+    
+    image = resize_image(image, new_width)
+    image = grayify(image)
+    
+    ascii_str = pixels_to_ascii(image)
+    img_width = image.width
+    
+    ascii_str_len = len(ascii_str)
+    ascii_img = ""
+    for i in range(0, ascii_str_len, img_width):
+        ascii_img += ascii_str[i:i+img_width] + "\n"
+    
+    with open("ascii_image.txt", "w") as f:
+        f.write(ascii_img)
+    return ascii_img
+
+def main(new_width=100):
+    path = "dog.png"
+    ascii_art = image_to_ascii(path, new_width=new_width)
+    print(ascii_art)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{Basic_ASCII_Art_generator}',
+ version='1.0',
+ author='{AIMalvin}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
